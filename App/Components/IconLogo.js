7a09e6e2e9fc1a3a12368d888deab6920be434ee
Lucas Pirola <lucaspirola@gmail.com>
2018-10-04 21:18:44 -0300

    Update icon and splash screen

---
+++ App/Components/IconLogo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+export default class SlackIcon extends React.Component {
+  render() {
+    return (
+      <Image
+        source={require('../Images/logo.png')}
+        fadeDuration={0}
+        style={{width: 25, height: 25}}
+      />
+    );
+  }
+}
