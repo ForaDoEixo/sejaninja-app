dfae5b960206014dc804f7331896fae87ace2361
Lucas Pirola <lucaspirola@gmail.com>
2018-10-04 21:24:14 -0300

    Add draft  from screens to feed and content

--- App/Containers/App.js
+++ App/Containers/App.js
@@ -1,13 +1,21 @@
 import '../Config'
 import DebugConfig from '../Config/DebugConfig'
 import React, { Component } from 'react'
-import { Provider } from 'react-redux'
+import { Provider as StoreProvider } from 'react-redux'
 import RootContainer from './RootContainer'
 import createStore from '../Redux'
+import { DarkTheme, Provider as PaperProvider } from 'react-native-paper'
 
 // create our store
 const store = createStore()
-
+const theme = {
+  ...DarkTheme,
+  colors: {
+    ...DarkTheme.colors,
+    primary: 'black',
+    accent: 'yellow',
+  },
+};
 /**
  * Provides an entry point into our application.  Both index.ios.js and index.android.js
  * call this component first.
@@ -20,9 +28,11 @@ const store = createStore()
 class App extends Component {
   render () {
     return (
-      <Provider store={store}>
-        <RootContainer />
-      </Provider>
+      <StoreProvider store={store}>
+        <PaperProvider theme={theme}>
+          <RootContainer />
+        </PaperProvider>
+      </StoreProvider>
     )
   }
 }
--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,32 +1,38 @@
-import React, { Component } from 'react'
-import { ScrollView, Text, Image, View } from 'react-native'
-import DevscreensButton from '../../ignite/DevScreens/DevscreensButton.js'
+import * as React from 'react';
+import { Appbar, BottomNavigation, Headline } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
 
-import { Images } from '../Themes'
+const MusicRoute = () => <Headline>Music</Headline>;
 
-// Styles
-import styles from './Styles/LaunchScreenStyles'
+const AlbumsRoute = () => <Headline>Albums</Headline>;
 
-export default class LaunchScreen extends Component {
-  render () {
+const RecentsRoute = () => <Headline>Recents</Headline>;
+
+export default class MyComponent extends React.Component {
+  state = {
+    index: 0,
+    routes: [
+      { key: 'music', title: 'Seja Ninja', icon: 'queue-music' },
+      { key: 'albums', title: 'Quem Somos', icon: 'home' },
+      { key: 'recents', title: 'Financie', icon: 'info' },
+    ],
+  };
+
+  _handleIndexChange = index => this.setState({ index });
+
+  _renderScene = BottomNavigation.SceneMap({
+    music: MusicRoute,
+    albums: AlbumsRoute,
+    recents: RecentsRoute,
+  });
+
+  render() {
     return (
-      <View style={styles.mainContainer}>
-        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
-        <ScrollView style={styles.container}>
-          <View style={styles.centered}>
-            <Image source={Images.launch} style={styles.logo} />
-          </View>
-
-          <View style={styles.section} >
-            <Image source={Images.ready} />
-            <Text style={styles.sectionText}>
-              This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
-            </Text>
-          </View>
-
-          <DevscreensButton />
-        </ScrollView>
-      </View>
-    )
+      <BottomNavigation
+        navigationState={this.state}
+        onIndexChange={this._handleIndexChange}
+        renderScene={this._renderScene}
+      />
+    );
   }
 }
